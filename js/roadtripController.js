30d69e8f4659fcd038e92abdda4076ecfebc6340
Justin Simonelli <justin.simonelli@gmail.com>
2016-03-15 21:42:23 -0700

    Just a quick update to change some content and data path

--- js/roadtripController.js
+++ js/roadtripController.js
@@ -1,5 +1,5 @@
 app.controller('TripController', function($scope, $http){
-  $http.get("/WestCoastRoadtrip/data.json")
+  $http.get("data.json")
     .then(function(response) {
         console.log(response.data.locations)
         $scope.locations = response.data.locations;
